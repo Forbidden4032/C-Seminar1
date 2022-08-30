316eee380be273320db7f0f17557fadfce715983
forbidden <forbidden@fedora.com>
2022-08-30 22:23:39 +0300

    add ex_2

---
+++ ex_2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите первое число: ");
+int int1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int int2 = int.Parse(Console.ReadLine());
+if (int1 > int2)
+Console.WriteLine($"Первое число {int1} больше второго числа {int2}");
+else
+Console.WriteLine($"Второе число {int2} больше первого числа {int1}");
