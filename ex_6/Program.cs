e67979824533795c6601ab0ec0947e6c1ce3659b
forbidden <forbidden@fedora.com>
2022-08-30 22:55:52 +0300

    add ex_6

---
+++ ex_6/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число: ");
+int int1 = int.Parse(Console.ReadLine());
+if (int1%2 == 0)
+Console.WriteLine($"Введенное число {int1} является четным");
+else
+Console.WriteLine($"Введенное число {int1} является нечетным");
