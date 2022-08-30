1ebe070ff9ea8bf5e7a7ed9c525899e3e7e651a7
forbidden <forbidden@fedora.com>
2022-08-30 23:35:51 +0300

    add ex_8

---
+++ ex_8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int int1 = int.Parse(Console.ReadLine());
+while (int1 > 1)
+    {
+    if (int1%2 == 0)
+    Console.WriteLine(int1);
+    int1--;
+    }
+    
+    
+
+
+
