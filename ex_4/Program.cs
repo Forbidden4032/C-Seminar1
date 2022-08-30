88023a32f6e7fad6b91d6abb7bee6542a067fb7a
forbidden <forbidden@fedora.com>
2022-08-30 22:45:12 +0300

    add ex_4

---
+++ ex_4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число: ");
+int int1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int int2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int int3 = int.Parse(Console.ReadLine());
+int max = int1;
+
+if (max < int2) max = int2;
+if (max < int3) max = int3;
+
+Console.WriteLine($"Максимальное из трех чисел: {max}");
+
+
